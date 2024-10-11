b91a4b5c9577319b822c31402b6c113c4f9a50ae
Ananya Doshi <88143582+ananyadoshi31@users.noreply.github.com>
2024-10-11 03:18:55 +0000

    third commit

--- task3.sh
+++ task3.sh
@@ -13,6 +13,6 @@
 # Finally, calculate the average age of the filtered passengers.
 # gawk -F, '{ ageSum += $7; counter += 1 } END { print ageSum / counter }'
 
-# All the parts piped together
+# All the parts piped 
 grep -E "^[0-9]{1,3},[0-9],2,.*,.*,.*,.*,.*,.*,.*,.*,S" titanic.csv | sed 's/female/F/; s/male/M/' | gawk -F, '$7 ~ /^[0-9]+(\.[0-9]+)?$/ { print; ageSum += $7; counter += 1 } END { if (counter > 0) print "\nAverage Age: " ageSum / counter; else print "No valid data found" }'
 exit
